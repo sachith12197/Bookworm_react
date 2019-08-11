41aae653f025447745f15a05aa03a6e40e3a738d
sachith12197 <lakshika.p@informaticsint.com>
2019-08-11 20:07:27 +0530

    Login form with client-side validation

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,66 +1,74 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Validator from 'validator';
-import { Form, Button} from "semantic-ui-react";
+import { Form, Button } from "semantic-ui-react";
 import InlineError from '../messges/InlineError';
 
-class LoginForm extends React.Component{
+class LoginForm extends React.Component {
     state = {
-        data:{
-            email:'',
-            password:''
+        data: {
+            email: '',
+            password: ''
         },
         loading: false,
-        errors:{}
+        errors: {}
     };
 
-onChange = e =>
-    this.state({
-        data:{...this.state.data, [e.target.name]: e.target.value}
-    });
+    onChange = e =>
+        this.setState({
+            data: { ...this.state.data, [e.target.name]: e.target.value }
+        });
 
-onSubmit = () => {
-    const errors = this.validate(this.state.data);
-    this.setState({ errors });
-};
+    onSubmit = () => {
+        const errors = this.validate(this.state.data);
+        this.setState({ errors });
+        if (Object.keys(errors).length === 0) {
+            this.props.submit(this.state.data);
+        }
+    };
 
-validate = (data) => {
-    const errors = {};
-    if(!Validator.isEmail(data.email)) errors.email = "Invalid email";
-    if(!data.password) errors.password = "Can't be blank";
-    return errors;
-};
+    validate = (data) => {
+        const errors = {};
+        if (!Validator.isEmail(data.email)) errors.email = "Invalid email";
+        if (!data.password) errors.password = "Can't be blank";
+        return errors;
+    };
 
-    render(){
+    render() {
         const { data, errors } = this.state;
 
-        return(
+        return (
             <Form onSubmit={this.onSubmit}>
-                <Form.Field error = {!!errors.email}>
+                <Form.Field error={!!errors.email}>
                     <label htmlFor="email">E-mail</label>
-                    <input type="email" 
-                           id="email" 
-                           name="email" 
-                           placeholder="example@example.com"
-                           value={data.email}
-                           onChange={this.onChange}/>
-                 {errors.email && <InlineError text={errors.email} />}   
-                 </Form.Field>
-                
-                 <Form.Field error = {!!errors.password}>
+                    <input type="email"
+                        id="email"
+                        name="email"
+                        placeholder="example@example.com"
+                        value={data.email}
+                        onChange={this.onChange} />
+                    {errors.email && <InlineError text={errors.email} />}
+                </Form.Field>
+
+                <Form.Field error={!!errors.password}>
                     <label htmlFor="password">Password</label>
-                    <input type="password" 
-                           id="password" 
-                           name="password" 
-                           placeholder="Make it secure"
-                           value={data.password}
-                           onChange={this.onChange}/>
-                 {errors.password && <InlineError text={errors.password} />}
-                 </Form.Field> 
-                   
-               <Button primary>Login</Button> 
+                    <input type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Make it secure"
+                        value={data.password}
+                        onChange={this.onChange} />
+                    {errors.password && <InlineError text={errors.password} />}
+                </Form.Field>
+
+                <Button primary>Login</Button>
             </Form>
         );
     }
+}
+
+LoginForm.propTypes = {
+    submit:PropTypes.func.isRequired
 }
 
 
